74f5acb0e58d3e8f2451e91d89654d6fc3ae36eb
snthebeast03 <88839441+snthebeast03@users.noreply.github.com>
2023-09-21 10:27:07 +0530

    Added prototype of PVE

--- pong game new.py
+++ pong game new.py
@@ -14,6 +14,7 @@ updown = random.choice([1,2,3,4,5,-1,-2,-3,-4,-5])
 ball_speed = 5
 paddle_speed = 10
 WHITE = (255,255,255)
+BLUE = (0,0,255)
 FPS = 60
 fps = pygame.time.Clock()
 screen = pygame.display.set_mode((800, 600))
@@ -41,7 +42,11 @@ class Paddles(pygame.sprite.Sprite):
                 self.rect.move_ip(0, -1*paddle_speed)
             if pressed_keys[K_s]:
                 self.rect.move_ip(0, paddle_speed)
-
+    def ai(self):
+        if updown > 0:
+            self.rect.move_ip(0, (paddle_speed-7))
+        if updown < 0:
+            self.rect.move_ip(0, -1*(paddle_speed-7))
 class Walls(pygame.sprite.Sprite):
     def __init__(self, pos):
         super().__init__() 
@@ -84,7 +89,7 @@ all_sprites.add(ball)
 q = True
 i=20
 start_time = time.time()
-flag = False
+flag_pvp, flag_pve = False, False
 while q:
     presses = pygame.key.get_pressed()
     for event in pygame.event.get():
@@ -94,25 +99,80 @@ while q:
             time.sleep(2)
             pygame.quit()
             sys.exit()
-    if flag == False:
-        screen.blit(splash_screen, (0,0))
-        press_space = bigfont.render('Press Space to Start', True, WHITE)
-        press_space_rect = press_space.get_rect()
-        press_space_rect.center = (401, 145)
-        press_esc = smallfont.render('Esc to Quit', True, WHITE)
-        press_esc_rect = press_esc.get_rect()
-        press_esc_rect.center = (400, 407)
-        screen.blit(press_space, press_space_rect)
-        screen.blit(press_esc, press_esc_rect)
+            
+    if flag_pvp == False and flag_pve == False:
+        screen.fill(WHITE)
+        pvp = bigfont.render('Space for P V P', True, BLUE)
+        pvp_rect = pvp.get_rect()
+        pvp_rect.center = (201, 145)
+
+        pve = bigfont.render('Space for P V Computer', True, BLUE)
+        pve_rect = pve.get_rect()
+        pve_rect.center = (551, 145)
+
+        esc = bigfont.render('Esc to Quit', True, BLUE)
+        esc_rect = esc.get_rect()
+        esc_rect.center = (400, 407)
+
+
+        screen.blit(pvp, pvp_rect)
+        screen.blit(pve, pve_rect)
+        screen.blit(esc, esc_rect)
         pygame.display.update()
-    if presses[K_SPACE] and flag == False:
-        flag = True
-    elif flag == True:
+    if presses[K_SPACE] and flag_pvp == False:
+        flag_pvp = True
+    if presses[K_RETURN] and flag_pve == False:
+        flag_pve = True
+    if flag_pvp == True:
         
         screen.fill(WHITE)
         for entity in all_sprites:
             screen.blit(entity.image, entity.rect)
             entity.move()
+        if ball.rect.colliderect(redplayer.rect) or ball.rect.colliderect(blackplayer.rect):
+            if red == 1:
+                red = 0
+                black = 1
+                if updown in range(1,6):
+                    updown = random.choice([1,2,3,4,5])
+                else:
+                    updown = random.choice([-1,-2,-3,-4,-5])
+            else:
+                red = 1
+                black = 0
+                if updown in range(1,6):
+                    updown = random.choice([1,2,3,4,5])
+                else:
+                    updown = random.choice([-1,-2,-3,-4,-5])
+        if ball.rect.colliderect(upwall.rect):
+            updown = random.randint(1,5)
+        if ball.rect.colliderect(downwall.rect):
+            updown = random.randint(-5,-1)
+
+        if ball.rect.center[0] > 820 or ball.rect.center[0] < -20:
+            screen.blit(end_game_screen, (0,0))
+            pygame.display.update()
+            time.sleep(2.5)
+            pygame.quit()
+            sys.exit()
+        
+        if round(time.time()-start_time)>1 and int(time.time()-start_time) % i == 0:
+            ball_speed += 1
+            paddle_speed += 1
+            i=i+20
+
+
+        pygame.display.update()
+        fps.tick(FPS)
+
+    if flag_pve == True:
+    
+        screen.fill(WHITE)
+        for entity in all_sprites:
+            screen.blit(entity.image, entity.rect)
+            entity.move()
+            if entity == blackplayer:
+                entity.ai()
         if ball.rect.colliderect(redplayer.rect) or ball.rect.colliderect(blackplayer.rect):
             if red == 1:
                 red = 0
